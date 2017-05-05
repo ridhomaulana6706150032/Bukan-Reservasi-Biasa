ce0790e77bcd52fe5724bbe6e72d918cae13b5b9
ridhomaulana6706150032 <ridhomaulana252@yahoo.com>
2017-05-06 03:48:19 +0700

    FINAL COMMIT

--- brb.sql
+++ brb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2017 at 03:51 PM
+-- Generation Time: May 05, 2017 at 05:57 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -19,6 +19,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `brb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meja`
+--
+
+CREATE TABLE IF NOT EXISTS `meja` (
+  `id_meja` varchar(5) NOT NULL,
+  `id_restoran` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_meja`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `meja`
+--
+
+INSERT INTO `meja` (`id_meja`, `id_restoran`) VALUES
+('A1', 'R0001'),
+('A2', 'R0001'),
+('A3', 'R0001'),
+('A4', 'R0002');
 
 -- --------------------------------------------------------
 
@@ -39,9 +61,59 @@ CREATE TABLE IF NOT EXISTS `register` (
 --
 
 INSERT INTO `register` (`nama`, `email`, `username`, `password`) VALUES
+('', '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
+('aras', 'ar@gmail.com', 'arsyan', 'e4cc65e5749b25cc212cee483e1dbf29'),
 ('Ridho Maulana Cahyudi', 'ridhomaulana252@yahoo.com', 'maulanaridho', '0d959d72214a4cf573a2a7d3c64f76f6'),
 ('Ridho Maulana Cahyudi', 'ridhomaulana252@yahoo.com', 'ridhomaulana', 'f5bb0c8de146c67b44babbf4e6584cc0'),
 ('Ridho Maulana Cahyudi', 'ridhomaulana252@yahoo.com', 'ridhomaulana1', '0d959d72214a4cf573a2a7d3c64f76f6');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservasi`
+--
+
+CREATE TABLE IF NOT EXISTS `reservasi` (
+  `id_reservasi` int(100) NOT NULL AUTO_INCREMENT,
+  `nama_cust` varchar(50) NOT NULL,
+  `id_restoran` varchar(50) NOT NULL,
+  `id_meja` varchar(5) NOT NULL,
+  `nama_restoran` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_reservasi`),
+  KEY `nama_restoran` (`nama_restoran`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Dumping data for table `reservasi`
+--
+
+INSERT INTO `reservasi` (`id_reservasi`, `nama_cust`, `id_restoran`, `id_meja`, `nama_restoran`) VALUES
+(10, 'ridho maulana', 'R0001', 'A1', ''),
+(11, 'fasfs', 'R0001', 'A1', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restoran`
+--
+
+CREATE TABLE IF NOT EXISTS `restoran` (
+  `id_restoran` varchar(10) NOT NULL,
+  `nama_restoran` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_restoran`),
+  UNIQUE KEY `nama_restoran` (`nama_restoran`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `restoran`
+--
+
+INSERT INTO `restoran` (`id_restoran`, `nama_restoran`) VALUES
+('R0004', 'Bakso Akung'),
+('R0002', 'Bakso Boedjangan'),
+('R0003', 'Nasi Goreng Mafia'),
+('R0001', 'Warunk Upnormal'),
+('R0005', 'Wendy''s');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- logout.php
+++ logout.php
@@ -3,24 +3,37 @@
 	session_start();
 	if (empty($_SESSION['username'])) {
 		header("location: login.php"); 
-	}
+	} else {
 ?>
 <html>
 <head>
 <title>DASHBOARD</title>
 <link rel='stylesheet' type='text/css' href='css/navbar-style.css'>
+<link rel='stylesheet' type='text/css' href='css/tes.css'>
+<link rel='stylesheet' type='text/css' href='css/sidebar.css'>
+<link rel='stylesheet' type='text/css' href='css/form.css'>
+<script type='text/javascript' src='ajax/ajax.js'></script>
 </head>
 <body>
 
 <ul>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#home">View Restaurants</a></li>
+  <li><a href="#home"></a></li>
+  <li><a href="#"> </a></li>
+  <li><a href="#"> </a></li>
   <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Settings</a>
+    <a href="javascript:void(0)" class="dropbtn"><?php 
+														include ("connection.php"); 
+														$sql = mysqli_query($conn, "SELECT * FROM register");
+														
+														if ($_SESSION['username']){
+															$user_login = $_SESSION['username']; 
+														}
+														echo $user_login;
+														?></a>
     <div class="dropdown-content">
     
       <a href="#">Profile</a>
-      <a href="#"><?php
+      <a href="logout.php"><?php
 					session_start(); 
 					session_destroy(); 
 					echo "<script>
@@ -32,8 +45,35 @@
   </li>
 </ul>
 <hr>
-<h3>Dropdown Menu inside a Navigation Bar</h3>
-<p>Hover over the "Dropdown" link to see the dropdown menu.</p>
-
+<div class="sidebar">
+    <h3 align ="center" color= "white">Dashboard</h3>
+	<ul>
+      <li><a href="index.php">Reservation</a></li>
+      <li><a href="viewdata.php">View Data</a></li>
+    </ul>
+  </div>
+  <div class="reservasi">
+				<form method = "get" action = "insert_data.php">
+						<label for = "cname"> Name Customer	</label><br>
+							<input type = "text" name = "namacust" placeholder= "Input Customer Name"><br>
+							<label for = "restoran"> Restaurant </label><br>
+								<select name = "nrestaurant" id="restaurant" onChange="showMeja(this.value)">
+								<option>-- Choose Restaurant--</option><br>
+								<?php
+									include 'connection.php';
+									$sql = mysqli_query($conn, "SELECT * FROM restoran");
+									while($data = mysqli_fetch_array($sql)){
+										echo "<option value='".$data['id_restoran']."'>(".$data['id_restoran'].")".$data['nama_restoran']."</option>";
+									}
+								?>
+</select><br>
+<div id = "meja">
+</div>
+  		<input type = "submit" name = "reservasi" value = "Reserve" class="btn btn=small btn-primary">
+	</form>		
+	</div>
 </body>
 </html>
+<?php
+}
+?>
