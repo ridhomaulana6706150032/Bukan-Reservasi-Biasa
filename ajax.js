e8fb61aa2ac6ddf00f4844f71efd5067779c76d8
Ridho Maulana Cahyudi <ridhomaulana252@yahoo.com>
2017-05-05 10:55:49 +0700

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,50 +1,33 @@
 var xmlHttp;
-function showMatkul(str){
-	xmlHttp = GetXmlHttpObject();
+function showMeja(str){
+	xmlHttp = GetXmlHttpObject()
 	if (xmlHttp == null){
-		alert("Tidak support request");
+		alert("Tidak support request")
 		return;
 	}
-	var url = "get_matkul.php?kodeprodi="+str;
-	xmlHttp.onreadystatechange = prodiChanged;
-	xmlHttp.open("GET",url,true);
-	xmlHttp.send(null);
+	var url = "get_meja.php"
+	url = url+"?meja="+str
+	xmlHttp.onreadystatechange = tableChanged
+	xmlHttp.open("GET",url,true)
+	xmlHttp.send(null)
 }
-function prodiChanged(){
+function tableChanged(){
 	if(xmlHttp.readyState == 4 || xmlHttp.readyState == "complete"){
-		document.getElementById("divMatkul").innerHTML = xmlHttp.responseText;
-	}
-}
-/* END OF OPTIONS*/
-
-function showDosen(str){
-	xmlHttp = GetXmlHttpObject();
-	if (xmlHttp == null){
-		alert("Tidak support request");
-		return;
-	}
-	var url = "get_dosen.php?kodematkul="+str;
-	xmlHttp.onreadystatechange = dosenChanged;
-	xmlHttp.open("GET",url,true);
-	xmlHttp.send(null);
-}
-function dosenChanged(){
-	if(xmlHttp.readyState == 4 || xmlHttp.readyState == "complete"){
-		document.getElementById("divDosen").innerHTML = xmlHttp.responseText;
+		document.getElementById("meja").innerHTML = xmlHttp.responseText
 	}
 }
 /* END OF OPTIONS*/
 
 function GetXmlHttpObject(){
-	var xmlHttp = null;
+	var xmlHttp = null
 	try{
-		xmlHttp = new XMLHttpRequest();
+		xmlHttp = new XMLHttpRequest()
 	}catch(e){
 		try{
-			xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+			xmlHttp = new ActiveXObject("Msxml2.XMLHTTP")
 		}catch(e){
-			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
 		}
 	}
-	return xmlHttp;
+	return xmlHttp
 }
